e377ee8fbd13ad5ebe7ca5facaa3adb456addc1f
Durva <durvadeshpande@gmail.com>
2025-06-11 18:21:00 +0530

    Connected to Mongo Cloud DB

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 function connectToDB(){
     mongoose.connect(process.env.MONGO_URL).then(() => {
-        console.log("Connected to DB")
+        console.log("Connected to Cloud")
     })
 }
 
