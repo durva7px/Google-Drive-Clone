ad6e9ad4769148d0d5bac100f204a5aad5c640e5
Durva <durvadeshpande@gmail.com>
2025-06-11 18:54:30 +0530

    Changed MONGO_URL

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 
 function connectToDB(){
-    mongoose.connect(process.env.MONGO_URL).then(() => {
+    mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }).then(() => {
         console.log("Connected to Cloud")
     })
 }
